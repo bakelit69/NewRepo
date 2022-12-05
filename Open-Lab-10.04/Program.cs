387cd03c0d38d7afabfb93b271a3478156b94ed4
bakelit69 <jakubchalupka385@gmail.com>
2022-12-05 18:48:59 +0100

    Starting

--- Open-Lab-10.04/Program.cs
+++ Open-Lab-10.04/Program.cs
@@ -1,15 +1,23 @@
-﻿using System;
+﻿using Open_Lab_10._03;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Open_Lab_10._04
+namespace Open_Lab_10._03
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Book LOTR = new Book();
+            LOTR.Title = "Lord Of The Rings";
+            LOTR.Pages = 576;
+            LOTR.Category = "Fantasy";
+            LOTR.Author = "J.R.R.Tolkien";
+            LOTR.ReleaseDate = 1954;
+            Console.WriteLine(LOTR.ToString());
         }
     }
 }
